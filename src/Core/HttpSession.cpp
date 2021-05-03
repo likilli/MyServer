ef4b7c1987167617a14e5026879ac9cd79dae21c
likilli <lk951208@gmail.com>
2021-05-03 23:27:03 +0800

    发送逻辑修改至Socket类中，HttpSession、Server部分功能移除，添加参数初始化，后续需要将接收功能分拆

--- src/Core/HttpSession.cpp
+++ src/Core/HttpSession.cpp
@@ -11,11 +11,12 @@
 #include <cstring>
 
 #include "HttpSession.hpp"
+#include "Event.hpp"
 
 
 constexpr int kBufSize = 4096;
 
-char http_header[] = "HTTP/1.1 200 OK\r\n"
+std::string kHeader = "HTTP/1.1 200 OK\r\n"
              "Connection: close\r\n"
              "Content-Type: text/html; charset=UTF-8\r\n"
              "Content-Length: 92\r\n"
@@ -24,76 +25,53 @@ char http_header[] = "HTTP/1.1 200 OK\r\n"
              "<!DOCTYPE html><html><head> Welcom, Kai</head><h1> aaa, elitk's Home page</h1></html>";
 
 
-HttpSession::HttpSession(const int fd) : fd_(fd)
-{}
+HttpSession::HttpSession(const int fd) : socket_(fd)
+{
+
+}
 
 
 HttpSession::~HttpSession()
 {
-    if (fd_ > 0)
-    {
-        shutdown(fd_, SHUT_RDWR);
-        ::close(fd_);
-    }
-
     if (!http_header_.empty())
         http_header_.clear();
 }
 
 
-int HttpSession::getFd() const
+void HttpSession::Read()
 {
-    return fd_;
+    OnRead();
 }
 
 
 void HttpSession::OnRead()
 {
     char buf[kBufSize]{};
-    ssize_t read_len = recv(fd_, buf, kBufSize - 1, 0);
+    ssize_t read_len = recv(socket_.GetFd(), buf, kBufSize - 1, 0);
     if (read_len == -1)
     {
         if (errno == EAGAIN || errno == EWOULDBLOCK)
         {
-            EventStart(fd_, EventType::Read, [this](){ OnRead(); });
+            EventStart(socket_.GetFd(), EventType::Read, [this](){ OnRead(); });
         }
     }
 
     std::string buffer(buf);
-    if (buffer.find("\r\n\r\n") == std::string::npos)
-        return;
-
     std::cout << buffer << std::endl;
 
-    EventStop(fd_, EventType::Read);
-    HttpSession::OnWrite();
+    EventStop(socket_.GetFd(), EventType::Read);
+    Send();
+}
+
+void HttpSession::Send()
+{
+    socket_.SetSendData(kHeader);
+    socket_.Send();
 }
 
 
-void HttpSession::OnWrite()
+void HttpSession::Close()
 {
-    ssize_t send_len = send(fd_, http_header + sent_len_, strlen(http_header), 0);
-    if (send_len == -1)
-    {
-        if (errno == EWOULDBLOCK || errno == EAGAIN)
-        {
-            EventStart(fd_, EventType::Write, [this](){OnWrite(); });
-        }
-    }
-    sent_len_ += send_len;
-    if (sent_len_ == static_cast<ssize_t>(strlen(http_header)))
-    {
-        EventStop(fd_, EventType::Write);
-        std::cout << "Send Finish" << std::endl;
-        shutdown(fd_, SHUT_RDWR);
-        ::close(fd_);
-        exit(0);
-    }
-}
-
-
-void HttpSession::Close() const
-{
-    shutdown(fd_, SHUT_RDWR);
-    ::close(fd_);
+    shutdown(socket_.GetFd(), SHUT_RDWR);
+    ::close(socket_.GetFd());
 }
--- src/Core/Socket.hpp
+++ src/Core/Socket.hpp
@@ -14,7 +14,11 @@
 class Socket
 {
 public:
+    using OnErrorCallBack = std::function<void(int)>;
+
+public:
     Socket();
+    explicit Socket(int fd);
     ~Socket();
 
 public:
@@ -22,10 +26,25 @@ public:
     void SetSendData(const std::string& data);
     std::string GetRecvData() const;
 
+    void Send();
+
+    socket_type GetFd() const { return socket_; }
+
+public:
+    void SetOnErrorCallBack(OnErrorCallBack on_error) { on_error_ = std::move(on_error); }
+
+private:
+    void OnSend();
+
+private:
+    std::size_t sent_len_{};
+
 private:
 
    socket_type socket_{};
 
    std::string send_buffer_{};
    std::string recv_buffer_{};
+
+   OnErrorCallBack on_error_{};
 };
