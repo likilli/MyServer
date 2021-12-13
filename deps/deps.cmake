143bc961131cd411bf8b7983752f6b58ef54d087
Li Kai <lk951208@gmail.com>
2021-12-13 09:26:29 +0800

    add gtest libraries for macOS (#4)

--- deps/deps.cmake
+++ deps/deps.cmake
@@ -1,2 +1,9 @@
+if (APPLE)
+    set(PLATFORM APPLE)
+elseif (UNIX)
+    set(PLATFORM Linux)
+endif()
+
+# gtest
 include_directories(${CMAKE_SOURCE_DIR}/deps/gtest/include)
-link_directories(${CMAKE_SOURCE_DIR}/deps/gtest)
+link_directories(${CMAKE_SOURCE_DIR}/deps/gtest/libs/${PLATFORM})
