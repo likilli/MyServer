80533abf9c8f96ab75be1d1efce0a98ed86c7237
LiKai <lk951208@gmail.com>
2022-02-07 23:39:19 +0800

    [Feature]: Add libevent static libraries and dynamic libraries

--- src/main.cc
+++ src/main.cc
@@ -1,16 +1,26 @@
 #include <iostream>
+#include <csignal>
 #include "server.h"
+
+
+void StopOnSignal(int signal)
+{
+    std::cout << "[INFO]: Receive signal: " << signal << std::endl;
+}
 
 
 int main()
 {
-    Server server;
-    if (!server.Start())
-    {
-        std::cerr << "Server start failed" << std::endl;
+    if (signal(SIGINT, StopOnSignal) == SIG_ERR) {
+        std::cout << "[INFO]: Set SIGINT Failed." << std::endl;
+    }
+    if (signal(SIGTERM, StopOnSignal) == SIG_ERR) {
+        std::cout << "[INFO]: Set SIGTERM Failed. " << std::endl;
     }
 
-	std::cout << "hello world" << std::endl;
+    Server server;
+    if (!server.Start())
+        std::cerr << "Server start failed" << std::endl;
 
     return 0;
 }
