cd627ec1841631fd3c50fd0df1f9d4d22659c791
LiKai <lk951208@gmail.com>
2022-02-13 21:49:40 +0800

    [Feature]: mv lib to libs, finish CMakeLists when USE_LIBEVENT

--- deps/deps.cmake
+++ deps/deps.cmake
@@ -7,3 +7,9 @@ endif()
 # gtest
 include_directories(${CMAKE_SOURCE_DIR}/deps/gtest/include)
 link_directories(${CMAKE_SOURCE_DIR}/deps/gtest/libs/${PLATFORM})
+
+if (USE_LIBEVENT)
+	include_directories(${CMAKE_SOURCE_DIR}/deps/libevent/include)
+	include_directories(${CMAKE_SOURCE_DIR}/deps/libevent/include/event2)
+	link_directories(${CMAKE_SOURCE_DIR}/deps/libevent/libs/${PLATFORM})
+endif()
