2f37520f59a3165cf975c2d7269187846035c3f4
Li Kai <lk951208@gmail.com>
2024-05-13 00:48:13 +0800

    Add OpenSSL files

--- deps/deps.cmake
+++ deps/deps.cmake
@@ -7,3 +7,8 @@ endif()
 # gtest
 include_directories(${CMAKE_SOURCE_DIR}/deps/gtest/include)
 link_directories(${CMAKE_SOURCE_DIR}/deps/gtest/libs/${PLATFORM})
+
+
+# OpenSSL
+include_directories(${CMAKE_SOURCE_DIR}/deps/openssl/include)
+link_directories(${CMAKE_SOURCE_DIR}/deps/openssl/libs/${PLATFORM})
